0bdae7bfdba60f0a5359bbfe5cd1b0895bc6af07
Jisu Kim <indexkim33@gmail.com>
2022-05-20 09:54:07 +0900

    #2 Math 클래스 메서드들

--- CSClass/Program.cs
+++ CSClass/Program.cs
@@ -23,6 +23,29 @@ namespace CSClass
 
             Console.WriteLine(random.NextDouble());
             Console.WriteLine(random.NextDouble()*10);
+
+            /*
+            List<int> list =new List<int>();
+            list.Add(1);
+            list.Add(222);
+            list.Add(4545);
+            */
+
+            List<int> list = new List<int>() { 51, 273, 62, 24 };
+            list.Remove(51); //51을 지움
+            foreach(var item in list)
+            {
+                Console.WriteLine("Count : "+list.Count+"\titem : "+item);
+            }
+            
+
+
+            Console.WriteLine(Math.Abs(-52273));
+            Console.WriteLine(Math.Ceiling(52.273));
+            Console.WriteLine(Math.Floor(52.273));
+            Console.WriteLine(Math.Max(52,273));
+            Console.WriteLine(Math.Min(52,273));
+            Console.WriteLine(Math.Round(52.273));
         }
     }
 }
