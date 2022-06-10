91b9fd25f8426ff5a25841293d659d910bbe8944
Jisu Kim <indexkim33@gmail.com>
2022-06-10 11:14:39 +0900

    #20 23-3. 다형성

--- CSClass/Program.cs
+++ CSClass/Program.cs
@@ -168,21 +168,15 @@ namespace CSClass
 
 
 
-            List<Dog> Dogs = new List<Dog> { new Dog(), new Dog(), new Dog() };
-            List<Cat> Cats = new List<Cat> { new Cat(), new Cat(), new Cat() };
+            // List<Dog> Dogs = new List<Dog> { new Dog(), new Dog(), new Dog() };
+            //List<Cat> Cats = new List<Cat> { new Cat(), new Cat(), new Cat() };
+            List<Animal> animals = new List<Animal> { new Dog(), new Cat() };
 
-            foreach(var item in Dogs)
+            foreach (var item in animals)
             {
                 item.Eat();
                 item.Sleep();
-                item.Bark();
-            }
-
-            foreach (var item in Cats)
-            {
-                item.Eat();
-                item.Sleep();
-                item.Meow();
+                //item.Meow();
             }
         }
     }
