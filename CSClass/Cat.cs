9ccdef13e973326d4cb85183ef7cf38f25ecfe28
Jisu Kim <indexkim33@gmail.com>
2022-06-10 10:45:31 +0900

    #18 23-1. 상속과 다형성 소개

---
+++ CSClass/Cat.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CSClass
+{
+    internal class Cat
+    {
+
+        public int Age { get; set; }
+        public Cat()
+        {
+            this.Age = 0;
+        }
+        public void Eat()
+        {
+            Console.WriteLine("고양이가 먹는다");
+        }
+
+        public void Sleep()
+        {
+            Console.WriteLine("고양이가 잔다");
+        }
+
+        public void Meow()
+        {
+            Console.WriteLine("고양이가 짖는다");
+        }
+    }
+}
---
+++ CSClass/Dog.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CSClass
+{
+    internal class Dog
+    {
+        public int Age { get; set; }
+        public string Color { get; set; }
+
+        public Dog()
+        {
+            this.Age = 0;
+        }
+        public void Eat()
+        {
+            Console.WriteLine("개가 먹는다");
+        }
+
+        public void Sleep()
+        {
+            Console.WriteLine("개가 잔다");
+        }
+
+        public void Bark()
+        {
+            Console.WriteLine("개가 짖는다");
+        }
+    }
+}
